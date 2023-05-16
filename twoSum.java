15944268b0d6a3f2f9e6d5226c4d1ac47457d529
dombonifacio <nikkibonifacio184@gmail.com>
2023-05-15 23:58:05 -0700

    Finished Two sum and passed test cases leetcode

--- twoSum.java
+++ twoSum.java
@@ -1,43 +1,39 @@
 class twoSum{
 
 
-    public void push(int[] newArr, int num){
-        for (int i = 0; i < newArr.length; i++){
-            newArr[i] = num;
-        }
-    }
+    // For visualization why j = 1 won't work
+    // [2, 7, 11, 9]
+    // outer loop: 2, inner loop: 7
+    // outer loop: 2, inner loop: 11
+    // outer loop: 2, inner loop: 9
+    // * * outer loop: 7, inner loop: 7 -> j will always start at index 1, allowing duplicate numbers
 
-    public int[] getTwoSum(int[] nums, int target){
-        // create an index that will define where the elements will be placed
+    public int[] getTwoSum(int[] nums, int target) {
         int[] newArr = new int[2];
         for (int i = 0; i < nums.length; i++){
-            for (int j = 1; j < nums.length - 1; j++){
+            for (int j = i + 1; j < nums.length; j++){
                 if (nums[i] + nums[j] == target){
-                    System.out.println(i);
-                    System.out.println(j);
-
                     newArr[0] = i;
                     newArr[1] = j;
+                    return newArr;
                 }
+                
                 
             }
         }
-        return new int[]{-1, -1};
+       return new int[]{-1, -1};
     }
 
     public static void main(String[] args){
 
-        int[] myArr = {11,15, 7, 2};
+        int[] myArr = {2, 7, 11, 9};
        // int[] newArr = new int[2];
         
         twoSum ans = new twoSum();
 
-        for ( int i =0; i < 5; i++){
-            for (int j = 1; j < 5; j++){
+       
 
-                System.out.println("i: " + i + " j: " + j);
-            }
-        }
+        
         
         
        // System.out.println(ans.getTwoSum(myArr, 9));
