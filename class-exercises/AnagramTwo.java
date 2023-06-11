074f15445c6e89c5ae06b4432b3bb23855bc91d8
dombonifacio <nikkibonifacio184@gmail.com>
2023-06-11 14:32:04 -0700

    Added Anagram checker technique (sorting then linear comparison)

---
+++ class-exercises/AnagramTwo.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class AnagramTwo {
+
+
+    public boolean IsAnagram(char[] wordOne, char[] wordTwo){
+
+        // sort wordOne and wordTwo
+        // linear compare them both
+
+        Arrays.sort(wordOne);
+        Arrays.sort(wordTwo);
+
+        // if length of both array do not match, it is not an anagram
+        if (wordOne.length != wordTwo.length){
+            return false;
+        }
+
+        int index = 0;
+        while (index < wordOne.length && wordOne[index] == wordTwo[index]){
+            index++;
+
+            // return true because done scanning for the whole array
+            if (index == wordOne.length){
+                return true;
+            }
+        }
+
+
+
+        return false;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+
+        String anagramOne = "race";
+        String anagramTwo = "care";
+        
+
+        // Our char array words
+        char[] charWordOne = anagramOne.toLowerCase().toCharArray();
+        char[] charWordTwo = anagramTwo.toLowerCase().toCharArray();
+
+        AnagramTwo sol = new AnagramTwo();
+
+        System.out.println(sol.IsAnagram(charWordOne, charWordTwo));
+    }
+    
+}
